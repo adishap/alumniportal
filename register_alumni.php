f0a231066b6bee9ef0ecb256549e2affa3266718
adishap <porwaladisha@gmail.com>
2014-12-27 16:18:16 +0530

    UI for registration

--- register_alumni.php
+++ register_alumni.php
@@ -20,120 +20,106 @@
   </head>
 
   <body>
-  
+  <script>
+    $('#sandbox-container input').datepicker({
+    });
+</script>
 
      <?php
 	 	include('header.php');
 	 ?>
+ 
+<form action="#" method="post" style="text-align:left;margin-left:5%">
+<h3>Register</h3>
+
 <div class="row">
-<div class="col-sm-12"  style="text-align:left; margin:5% 0 5% 7%">	 
-<form action="#" method="post">
-<h3> Log In</h3>
-<label for="input1">User Name </label><br>
-<input name="user_id" type="text" placeholder="Username" id="input1" required /><br><br>
-<label for="input2">Password</label><br>
-<input name="password" type="password" placeholder="Password" id="input2" required/><br><br>
- <input type="hidden" name="login" value="login">
-<button type="submit" class="btn btn-primary" style="text-align:center">LogIn</button><br><br>
-Not Registered Yet?
-<a href="register_alumni.php">Register</a> today.
+<label>Name </label><br>
+<div class="col-sm-3"  >	
+<input name="first_name" type="text" placeholder="First Name"required />
+</div>
+<div class="col-sm-3"  >
+<input name="middle_name" type="text" placeholder="Middle Name" />
+</div>
+<div class="col-sm-3"  >
+<input name="last_name" type="text" placeholder="Last Name" required />
+</div>
+<div class="col-sm-3"  >
+<!--intentionally blank-->
+</div>
+</div >
+<br><br>
+<label>Gender</label><br>
+<div class="row">
+<div class="col-sm-2"  >
+<input type="radio" name="sex" value="male" checked>Male
+</div>
+<div class="col-sm-2"  >
+<input type="radio" name="sex" value="female">Female
+</div>
+<div class="col-sm-2"  >
+<input type="radio" name="sex" value="other">other
+</div>
+<div class="col-sm-6"  >
+<!--intentionally blank-->
+</div>
+</div>
+<br><br>
+<label>Date of Birth</label><br>
+    <input type="text" type="text" required/>
+<br><br>
+<label>Course</label><br>
+ <select name="course" >
+  <option value="M.Tech(IT)">M.Tech(IT)</option>
+  <option value="B.com(Hons)">B.com(Hons)</option>
+  <option value="MCA (6 Years)">MCA (6 Years)</option>
+  <option value="MBA(Management Science)-5 Yrs">MBA(Management Science)-5 Yrs</option>
+<option value="MBA(Management Science)-2 Yrs">MBA(Management Science)-2 Yrs</option>
+<option value="MBA(Tourism Administration)">MBA(Tourism Administration)</option>
+<option value="MBA(APR)">MBA(APR)</option>
+</select>
+<br><br>
+<div class="row">
+<div class="col-sm-6">
+<label>Admission Year</label><br>
+<select name="admission_year" value ="<?php echo $year ;?>">
+<script>var i;for(i=1990; i<=new Date().getFullYear(); i++) {document.write("<option>"+i + "</option>");}</script>
+</select>
+</div>
+<div class="col-sm-6">
+<label>Passing Year</label><br>
+<select name="passing_year" value ="<?php echo $year ;?>">
+<script>var i;for(i=1991; i<=new Date().getFullYear(); i++) {document.write("<option>"+i + "</option>");}</script>
+</select>
+<br><br>
+</div>
+</div>
+<div class="row">
+<div class="col-sm-4">
+<label>Mobile Number</label><br>
+<input name="mobile_no" type="text" placeholder="+91-9876543210"required />
+</div>
+<div class="col-sm-8">
+<label>Email</label><br>
+<input name="email_id" type="email" placeholder="abc@de.f"required />
+</div>
+</div>
+<br><br>
+<div class="row">
+<label>Current City</label><br>
+<input name="city" type="text" placeholder="Mumbai" required />
+</div>
+<br><br>
+<label>Choose Password</label>
+<input type="password" name="new_password" style="margin-left:2%" placeholder="Password" required>
+<br>
+<label>Re-enter Password</label>
+<input type="password" name="enter_password" style="margin-left:1%" placeholder="Re-enter Password" required>
+<br><br>
+ <input type="hidden" name="register" value="register">
+<button type="submit" class="btn btn-primary" >Register</button><br><br>
+Already registered?
+<a href="log_in.php">Sign in</a> now.
 </form>
 
-<?php
-	     require 'db_connect.php';
-		 
-	   if(isset($_POST['login'])){
-		$user_id = $_POST['user_id'];
-		$password = md5($_POST['password']);
-		
-		$login_query = "SELECT * FROM `user_login` WHERE `user_id`= '".$user_id."' AND `password`='".$password."'";
-		 if($query_run = mysql_query($login_query)){
-				$query_num_rows = mysql_num_rows($query_run);
-				if($query_num_rows == 0){
-					echo "Enter a valid username and password.";
-					mysql_close($con);
-					}
-				else{
-					$user_id = mysql_result($query_run,0,'fusername');
-					$_SESSION['user_id'] = $user_id;
-					header('location:profile.php');
-					}
-		 }
-		 else{
-			 echo "Error Occurred.";
-			 }
-	   }
-?>
-
-</div>
-</div>
- 
- <?php
-		include('footer.php');
-		include('jsLinks.php');
-		include('slider_jsLinks.php');
-	 ?>
-	 
-	
-	
-	<!-- Script for "News/Events" tab -->
-	<script src="assets/js/holder.js"></script>
-	<?php 
-
-		$browser = get_browser(null, true) ;
-		$browser =  strtolower($browser['browser']) ; 
-
-		if ($browser == 'ie') {
-		echo " 
-			<script>
- 		 $(function () {
-  			$('#myTab a').click(function (e) {
-    			e.preventDefault();
-  				$(this).tab('show');
-			})
-
-    	    $('#myTab a:first').tab('show');
-
- 			$(window).load(function(){
- 			        $('#myModal').modal('show');
- 			    });	   	    
- 		 })
-	</script>
-
-		";
-
-
-		}
-				else {
-					echo " 
-					<script>
-		 		 $(function () {
-		  			$('#myTab a').click(function (e) {
-		    			e.preventDefault();
-		  				$(this).tab('show');
-					})
-
-		    	    $('#myTab a:first').tab('show');
-
-		 				   	    
-		 		 })
-			</script>
-
-				";
-			}
-	?>
-	
-  <!-- Script for testimonial -->
-	<script>
-	$(document).ready(function() {
-            $('ul#quotes').quote_rotator();
-            $('ul#button_quotes').quote_rotator();
-        });
-
-
-   </script>
-
-   <script src="https://www.surveymonkey.com/jsPop.aspx?sm=mM0njMJ6P3CyzaMh0OhMew_3d_3d"> </script>
-  
   </body>
 </html>
