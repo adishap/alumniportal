09d23c5986dbac8289d0f931cad4435a84a649b6
adishap <porwaladisha@gmail.com>
2016-05-29 19:30:21 +0530

    small fixes

---
+++ alumni_menu.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">IIPS Alumni Portal</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="alumni_association.php">IIPS Alumni Association</a></li>
+	<li><a href="alumni_gallary.php">Photo Gallery</a></li>
+	<li><a href="alumni_achievement.php">Extraordinary Achievements</a></li>
+	<li class="dropdown">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown" style="color: white;">Profile<b class="caret"></b></a>
+		<ul class="dropdown-menu">
+			<li><a href="register_alumni.php" >Register</a></li>
+			<li><a href="log_in.php">Log In</a></li>							
+			<li><a href="alumni_profile.php">Profile</a></li>
+                        <li><a href="alumni_update_profile.php">Update Profile</a></li> 
+                        <li><a href="alumni_search.php">Search a Friend</a></li>
+                        <li><a href="log_out.php">Log Out</a></li> 
+                        <li><a href="alumni_FAQ.php">FAQ</a></li>
+		</ul>
+	</li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
